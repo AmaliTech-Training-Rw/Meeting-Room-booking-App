1e0b30310215dc1cc732a27aaa7ef441c41d3789
cadet <cadet.kamdaou@amalitech.org>
2023-05-18 16:15:11 +0200

    Added splash animation

---
+++ app/src/main/java/com/amalitech/bookmeetingroom/ui/components/SplashAnimation.kt
@@ -0,0 +1,81 @@
+package com.amalitech.bookmeetingroom.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.amalitech.bookmeetingroom.R
+import com.amalitech.bookmeetingroom.ui.theme.PrimaryTextColor
+import com.amalitech.bookmeetingroom.ui.theme.seed
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashAnimation(
+    onNavigate: () -> Unit,
+    backgroundColor: Color = PrimaryTextColor,
+    tintColor: Color = seed,
+    drawable: Painter = painterResource(id = R.drawable.logo),
+    canShowFadeoutAnim: MutableState<Boolean>,
+    onVisibilityChange: (Boolean) -> Unit
+
+) {
+    val scale = remember {
+        Animatable(15f)
+    }
+
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            1f,
+            animationSpec = tween(
+                durationMillis = 1000,
+            )
+        )
+        delay(2000)
+        onVisibilityChange(false)
+        delay(500)
+        onNavigate()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center,
+    ) {
+        AnimatedVisibility(
+            visible = canShowFadeoutAnim.value,
+            exit = fadeOut(
+                animationSpec = tween(
+                    500
+                )
+            ),
+            enter = EnterTransition.None
+        ) {
+            Image(
+                painter = drawable,
+                contentDescription = stringResource(id = R.string.logo),
+                modifier = Modifier
+                    .scale(scale.value),
+                colorFilter = ColorFilter.tint(tintColor)
+            )
+        }
+    }
+}
