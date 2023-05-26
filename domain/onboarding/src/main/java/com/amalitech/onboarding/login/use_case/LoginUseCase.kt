01432fad79e5bb1a60b0a0b46b88426701f695f6
Cadet Kamdaou <cadet.kamdaou@amalitech.org>
2023-05-26 13:11:28 +0200

    removed unsed imports

--- domain/onboarding/src/main/java/com/amalitech/onboarding/login/use_case/LoginUseCase.kt
+++ domain/onboarding/src/main/java/com/amalitech/onboarding/login/use_case/LoginUseCase.kt
@@ -1,7 +1,7 @@
 package com.amalitech.onboarding.login.use_case
 
 data class LoginUseCase(
-    val logIn: com.amalitech.onboarding.login.use_case.LogIn,
-    val validateEmail: com.amalitech.onboarding.login.use_case.ValidateEmail,
-    val validatePassword: com.amalitech.onboarding.login.use_case.ValidatePassword
+    val logIn: LogIn,
+    val validateEmail: ValidateEmail,
+    val validatePassword: ValidatePassword
 )
