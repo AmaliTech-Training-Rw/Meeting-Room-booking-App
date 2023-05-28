35a3aee9064d8800a4b8d706fac487249154b252
ma-za-kpe <makpalyy@gmail.com>
2023-05-29 00:15:27 +0300

    remove both both the LoginScreen call in mainActivity and BookMeetingRoomDrawer(appState)

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -3,34 +3,14 @@ package com.amalitech.bookmeetingroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.amalitech.core_ui.state.rememberBookMeetingRoomAppState
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
-import com.amalitech.core_ui.components.BookMeetingRoomDrawer
-import com.amalitech.onboarding.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookMeetingRoomTheme {
-                // TODO: uncomment this while testing, and move to appropriate position ...
-                // val appState = rememberBookMeetingRoomAppState()
-                // BookMeetingRoomDrawer(appState)
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LoginScreen(
-                        onNavigateToHome = { /*TODO*/ },
-                        onNavigateToForgotPassword = { /*TODO*/ },
-                        onNavigateToSignUp = { /*TODO*/ },
-                        onNavigateUp = { /*TODO*/ })
-                }
+
             }
         }
     }
