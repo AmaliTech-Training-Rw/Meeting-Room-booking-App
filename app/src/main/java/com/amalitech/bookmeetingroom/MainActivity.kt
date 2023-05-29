04b1862472c095b88fd2bbfb48a16ea065b8a51f
ma-za-kpe <makpalyy@gmail.com>
2023-05-29 15:34:45 +0300

    add dark and light mode colors

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -3,14 +3,17 @@ package com.amalitech.bookmeetingroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.amalitech.core_ui.components.drawer.BookMeetingRoomDrawer
+import com.amalitech.core_ui.state.rememberBookMeetingRoomAppState
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val appState = rememberBookMeetingRoomAppState()
             BookMeetingRoomTheme {
-
+                BookMeetingRoomDrawer(appState)
             }
         }
     }
