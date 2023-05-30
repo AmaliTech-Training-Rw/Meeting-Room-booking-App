6e8e6a58c53bef26ca3d221e2e5f268d54f1ba49
Cadet Kamdaou <cadet.kamdaou@amalitech.org>
2023-05-30 12:25:02 +0200

    removed reset password from MainActivity

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -10,9 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.amalitech.onboarding.login.LoginScreen
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
-import com.amalitech.onboarding.reset_password.ResetPasswordScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ResetPasswordScreen() {}
+                    Greeting(name = "Android")
                 }
             }
         }
