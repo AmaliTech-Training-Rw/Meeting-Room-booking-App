7eadc5c3a12047b71da44f71e66ecbe143d27f74
Cadet Kamdaou <cadet.kamdaou@amalitech.org>
2023-05-30 12:13:18 +0200

    Resolved conflicts and removed bottom nav from MainActivity

--- app/src/main/java/com/amalitech/bookmeetingroom/BookMeetingApplication.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/BookMeetingApplication.kt
@@ -3,8 +3,9 @@ package com.amalitech.bookmeetingroom
 import android.app.Application
 import com.amalitech.core.di.coreModule
 import com.amalitech.core.di.networkModule
-import com.example.onboarding_data.di.onboardingDataModule
-import com.example.onboarding_presentation.di.onboardingPresentationModule
+import com.amalitech.onboarding.di.onboardingDomainModule
+import com.amalitech.onboarding.di.onboardingPresentationModule
+import com.amalitech.onboarding_data.di.onboardingDataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
@@ -18,6 +19,7 @@ class BookMeetingApplication : Application() {
                 networkModule,
                 onboardingDataModule,
                 onboardingPresentationModule,
+                onboardingDomainModule
             )
         }
     }
