321c4b6af52c65bbcdfe313021778bd72b83e139
ma-za-kpe <makpalyy@gmail.com>
2023-05-31 12:36:32 +0300

    clea code

--- core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomApp.kt
+++ core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomApp.kt
@@ -3,8 +3,8 @@ package com.amalitech.core_ui.ui
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.amalitech.core_ui.navigation.BookMeetingRoomNavHost
-import com.amalitech.core_ui.navigation.homeNavigationRoute
 import com.amalitech.core_ui.state.BookMeetingRoomAppState
+import com.amalitech.core_ui.state.NavigationItem
 
 @Composable
 fun BookMeetingRoomApp(
@@ -22,7 +22,7 @@ fun BookMeetingRoomApp(
         content = { innerPadding ->
             BookMeetingRoomNavHost(
                         innerPadding,
-                homeNavigationRoute,
+                NavigationItem.Home.route,
                         appState
                     )
                 }
