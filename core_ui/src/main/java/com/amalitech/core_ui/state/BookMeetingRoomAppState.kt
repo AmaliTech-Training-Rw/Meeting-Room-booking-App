b500160c0a7b665b6f8934f8c2672dc2ac8d7a09
ma-za-kpe <makpalyy@gmail.com>
2023-06-05 14:29:56 +0300

    add initial snackbar manager logic

--- core_ui/src/main/java/com/amalitech/core_ui/state/BookMeetingRoomAppState.kt
+++ core_ui/src/main/java/com/amalitech/core_ui/state/BookMeetingRoomAppState.kt
@@ -1,20 +1,28 @@
 package com.amalitech.core_ui.state
 
+import android.content.res.Resources
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.amalitech.core_ui.util.SnackbarManager
+import com.amalitech.core_ui.util.SnackbarMessage.Companion.toMessage
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 
 @Composable
 fun rememberBookMeetingRoomAppState(
@@ -25,6 +33,8 @@ fun rememberBookMeetingRoomAppState(
     ),
     systemUiController: SystemUiController = rememberSystemUiController(),
     snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
+    snackbarManager: SnackbarManager = SnackbarManager,
+    resources: Resources = resources(),
 ): BookMeetingRoomAppState {
     return remember(
         navController,
@@ -32,25 +42,47 @@ fun rememberBookMeetingRoomAppState(
         drawerState,
         systemUiController,
         snackbarHostState,
+        snackbarManager,
+        resources
     ) {
         BookMeetingRoomAppState(
             navController,
             coroutineScope,
             drawerState,
             systemUiController,
-            snackbarHostState
+            snackbarHostState,
+            snackbarManager,
+            resources
         )
     }
+}
+
+@Composable
+@ReadOnlyComposable
+fun resources(): Resources {
+    LocalConfiguration.current
+    return LocalContext.current.resources
 }
 
 @Stable
 class BookMeetingRoomAppState(
     val navController: NavHostController,
-    val coroutineScope: CoroutineScope,
+    coroutineScope: CoroutineScope,
     val drawerState: DrawerState,
     val systemUiController: SystemUiController,
     val snackbarHostState: SnackbarHostState,
+    val snackbarManager: SnackbarManager,
+    val resources: Resources
 ) {
+
+    init {
+        coroutineScope.launch {
+            snackbarManager.snackbarMessages.filterNotNull().collect { snackbarMessage ->
+                val text = snackbarMessage.toMessage(resources)
+                snackbarHostState.showSnackbar(text)
+            }
+        }
+    }
 
     val currentDestination: NavDestination?
         @Composable get() = navController
@@ -87,5 +119,4 @@ class BookMeetingRoomAppState(
             popUpTo(0) { inclusive = true }
         }
     }
-
 }
--- core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomApp.kt
+++ core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomApp.kt
@@ -1,7 +1,13 @@
 package com.amalitech.core_ui.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.amalitech.core_ui.navigation.BookMeetingRoomNavHost
 import com.amalitech.core_ui.state.BookMeetingRoomAppState
 import com.amalitech.core_ui.state.NavigationItem
@@ -21,11 +27,22 @@ fun BookMeetingRoomApp(
         },
         content = { innerPadding ->
             BookMeetingRoomNavHost(
-                        innerPadding,
+                innerPadding,
                 NavigationItem.Home.route,
-                        appState
+                appState
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = appState.snackbarHostState,
+                modifier = Modifier.padding(8.dp),
+                snackbar = { snackbarData ->
+                    Snackbar(
+                        snackbarData, contentColor = MaterialTheme.colorScheme.onPrimary
                     )
                 }
             )
+        }
+    )
 
 }
