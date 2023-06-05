554d0b495b4aa2deef81533d17c1722fa238c95e
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-06-06 00:19:24 +0200

    retain MainActivity as develop is

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -3,12 +3,8 @@ package com.amalitech.bookmeetingroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
 
@@ -17,13 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BookMeetingRoomTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
