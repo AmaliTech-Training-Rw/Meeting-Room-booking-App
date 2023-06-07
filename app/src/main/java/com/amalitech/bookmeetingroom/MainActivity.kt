e21312921f1fbd86558191e65cce68559cba44f4
Cadet Kamdaou <cadet.kamdaou@amalitech.org>
2023-06-07 10:25:46 +0200

    Removed AppScaffold call

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,8 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BookMeetingRoomTheme {
-                val shouldShowBoarding by viewModel.showOnBoarding.collectAsStateWithLifecycle()
-                AppScaffold(shouldShowBoarding)
+                Greeting(name = "Android")
             }
         }
     }
