d67ea58d9f6488f84155a5c7f1c9db3c0531db6d
ma-za-kpe <makpalyy@gmail.com>
2023-06-07 11:47:53 +0300

    add comment to mainvm

--- core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomViewModel.kt
+++ core_ui/src/main/java/com/amalitech/core_ui/ui/BookMeetingRoomViewModel.kt
@@ -10,6 +10,11 @@ import kotlinx.coroutines.launch
 
 open class BookMeetingRoomViewModel() : ViewModel() {
     fun launchCatching(snackbar: Boolean = true, block: suspend CoroutineScope.() -> Unit) =
+        /**
+         * A [CoroutineExceptionHandler] can be called from any thread. So, if [block] is supposed to
+         * run in the main thread, you need to be careful and call this function on a scope that
+         * runs in the main thread, such as a [viewModelScope] for viewmodels.
+         */
         viewModelScope.launch(
             CoroutineExceptionHandler { _, throwable ->
                 if (snackbar) {
