beadad73cae2763cbd52492932e777764f4e2137
Cadet Kamdaou <cadet.kamdaou@amalitech.org>
2023-07-07 17:23:48 +0200

    Showed selected date on the time picker Rewrote texts in the UI Centered components in row Gave fixed size to all selectable times Displayed all times and disabled non-available ones Adjusted tests Reset endTimes value everytime a new start time is selected

--- ui/home/src/main/java/com/amalitech/home/book_room/SlotSelectionManager.kt
+++ ui/home/src/main/java/com/amalitech/home/book_room/SlotSelectionManager.kt
@@ -1,10 +1,8 @@
 package com.amalitech.home.book_room
 
-import java.time.LocalTime
-
 data class SlotSelectionManager(
-    val availableStartTimes: List<LocalTime> = emptyList(),
-    val availableEndTimes: List<LocalTime> = emptyList(),
+    val availableStartTimes: List<TimeUi> = emptyList(),
+    val availableEndTimes: List<TimeUi> = emptyList(),
     val canShowStartTimes: Boolean = false,
     val canShowEndTimes: Boolean = false
 )
---
+++ ui/home/src/main/java/com/amalitech/home/book_room/TimeUi.kt
@@ -0,0 +1,8 @@
+package com.amalitech.home.book_room
+
+import java.time.LocalTime
+
+data class TimeUi(
+    val time: LocalTime,
+    val isAvailable: Boolean
+)
