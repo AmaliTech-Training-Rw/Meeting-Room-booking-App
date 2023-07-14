9d092feda6723aa5dc99f8460eafb923fd29a6d2
ma-za-kpe <makpalyy@gmail.com>
2023-07-14 13:18:00 +0300

    add initial list item design

--- ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
+++ ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
@@ -2,25 +2,30 @@ package com.amalitech.home.room
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.painter.Painter
@@ -33,9 +38,7 @@ fun RoomScreen() {
     val list = listOf(
         "A", "B", "C", "D"
     ) + ((0..10).map { it.toString() })
-    LazyColumn(
-
-    ) {
+    LazyColumn() {
         items(items = list, itemContent = { item ->
             RoomItem(item)
         })
@@ -44,54 +47,52 @@ fun RoomScreen() {
 
 @Composable
 fun RoomItem(item: String) {
-    Row(
+    Card(
         modifier = Modifier
-            .padding(10.dp)
+            .fillMaxWidth()
+            .padding(0.dp, 15.dp)
+            .clickable { },
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
     ) {
-        val image: Painter = painterResource(id = R.drawable.room)
-        Image(
-            painter = image,
-            contentDescription = item,
+        Row(
+            horizontalArrangement = Arrangement.Start,
             modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .clip(RoundedCornerShape(10.dp))
-        )
-        Column(
-            Modifier.weight(2f).background(Yellow)
+                .fillMaxSize()
         ) {
-            Text(text = "Weight = 2")
+            val image: Painter = painterResource(id = R.drawable.room)
+            Image(
+                painter = image,
+                contentDescription = item,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .clip(RoundedCornerShape(10.dp))
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            RoomName()
         }
     }
-//    Surface(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .height(183.dp)
-//            .clickable { },
-//        shape = RoundedCornerShape(10.dp),
-//        shadowElevation = 5.dp,
-//    ) {
-//        Row(
-//            horizontalArrangement = Arrangement.SpaceEvenly
-//        ) {
-//            val image: Painter = painterResource(id = R.drawable.room)
-//            Image(
-//                painter = image,
-//                contentDescription = item,
-//                modifier = Modifier
-//                    .weight(1f)
-//                    .width(195.dp)
-//                    .fillMaxHeight()
-//                    .clip(RoundedCornerShape(10.dp))
-//            )
-//            Spacer(modifier = Modifier.height(32.dp))
-//            Text(
-//                "Hello World!2",
-//                modifier = Modifier
-//                    .weight(2f)
-//            )
-//        }
-//    }
+}
+
+@Composable
+fun RoomName() {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+//            .border(2.dp, Blue)
+            .fillMaxSize()
+    ) {
+        val image: Painter = painterResource(id = R.drawable.line)
+        Image(
+            painter = image,
+            contentDescription = "line",
+            modifier = Modifier.fillMaxHeight()
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text("Hello World!")
+            Text("Hello World!2")
+        }
+    }
 }
 
 
