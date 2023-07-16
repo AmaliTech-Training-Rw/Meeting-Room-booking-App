c538113780a7f83b97e12690cb7618fee056120c
ma-za-kpe <makpalyy@gmail.com>
2023-07-16 16:19:53 +0300

    update room item list

--- ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
+++ ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,7 +31,12 @@ import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.Dimension
 import com.amalitech.core_ui.R
 
 @Composable
@@ -57,6 +63,8 @@ fun RoomItem(item: String) {
         Row(
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.surface)
                 .fillMaxSize()
         ) {
             val image: Painter = painterResource(id = R.drawable.room)
@@ -64,8 +72,9 @@ fun RoomItem(item: String) {
                 painter = image,
                 contentDescription = item,
                 modifier = Modifier
-                    .fillMaxHeight()
+                    .height(100.dp)
                     .clip(RoundedCornerShape(10.dp))
+                    .padding(0.dp, 15.dp)
             )
             Spacer(modifier = Modifier.width(16.dp))
             RoomName()
@@ -89,8 +98,31 @@ fun RoomName() {
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text("Hello World!")
-            Text("Hello World!2")
+            Text(
+                text = "Room Name",
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.W700,
+                fontSize = 13.sp
+            )
+            Text(
+                text = "Up to 6 people",
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.W300,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Air conditioning, Internet, Whiteboard, Natural light, Drinks",
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.W300,
+                fontSize = 12.sp
+            )
         }
     }
 }
