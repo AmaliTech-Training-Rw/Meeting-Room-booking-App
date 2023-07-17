f655648498d5253e6878ae9ea0d4a4fedcd72c43
ma-za-kpe <makpalyy@gmail.com>
2023-07-17 13:11:14 +0300

    update room item list

--- ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
+++ ui/home/src/main/java/com/amalitech/home/room/RoomScreen.kt
@@ -2,8 +2,6 @@ package com.amalitech.home.room
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -21,22 +20,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.Dimension
 import com.amalitech.core_ui.R
 
 @Composable
@@ -44,7 +39,12 @@ fun RoomScreen() {
     val list = listOf(
         "A", "B", "C", "D"
     ) + ((0..10).map { it.toString() })
-    LazyColumn() {
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp, 16.dp)
+    ) {
         items(items = list, itemContent = { item ->
             RoomItem(item)
         })
@@ -55,29 +55,46 @@ fun RoomScreen() {
 fun RoomItem(item: String) {
     Card(
         modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
             .fillMaxWidth()
-            .padding(0.dp, 15.dp)
-            .clickable { },
-        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+            .padding(5.dp, 10.dp),
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
     ) {
         Row(
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier
-                .clip(RoundedCornerShape(10.dp))
                 .background(MaterialTheme.colorScheme.surface)
+                .clip(RoundedCornerShape(10.dp))
                 .fillMaxSize()
+                .wrapContentHeight()
         ) {
             val image: Painter = painterResource(id = R.drawable.room)
             Image(
                 painter = image,
-                contentDescription = item,
-                modifier = Modifier
-                    .height(100.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .padding(0.dp, 15.dp)
+                contentDescription = null,
+                modifier = Modifier,
             )
-            Spacer(modifier = Modifier.width(16.dp))
-            RoomName()
+
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .padding(8.dp)
+            ) {
+                RoomName()
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Air conditioning, Internet, Whiteboard, Natural light, Drinks",
+                    modifier = Modifier,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.W300,
+                    fontSize = 12.sp
+                )
+            }
         }
     }
 }
@@ -87,14 +104,13 @@ fun RoomName() {
     Row(
         horizontalArrangement = Arrangement.Start,
         modifier = Modifier
-//            .border(2.dp, Blue)
             .fillMaxSize()
     ) {
-        val image: Painter = painterResource(id = R.drawable.line)
-        Image(
-            painter = image,
-            contentDescription = "line",
-            modifier = Modifier.fillMaxHeight()
+        Divider(
+            modifier = Modifier
+                .size(3.dp, 40.dp)
+                .padding(0.dp, 4.dp)
+                .background(MaterialTheme.colorScheme.primary)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
@@ -114,18 +130,6 @@ fun RoomName() {
                 fontWeight = FontWeight.W300,
                 fontSize = 16.sp
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Air conditioning, Internet, Whiteboard, Natural light, Drinks",
-                modifier = Modifier,
-                color = MaterialTheme.colorScheme.onSurface,
-                textAlign = TextAlign.Start,
-                fontWeight = FontWeight.W300,
-                fontSize = 12.sp
-            )
         }
     }
 }
-
-
-
---
+++ ui/home/src/main/java/com/amalitech/home/room/RoomUiState.kt
@@ -0,0 +1,8 @@
+package com.amalitech.home.room
+
+data class RoomUiState(
+    val room_name: String,
+    val number_of_people: Int,
+    val description: String,
+    val imageRes : Int
+)
