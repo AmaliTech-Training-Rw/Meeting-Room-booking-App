a5f5b26d2bcb12a83a2b385e64ee8c970809fb72
Cadet Kamdaou <nkamdaou@gmail.com>
2023-08-09 11:23:36 +0200

    Added appscaffold

--- app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
+++ app/src/main/java/com/amalitech/bookmeetingroom/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.amalitech.bookmeetingroom
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.amalitech.core_ui.theme.BookMeetingRoomTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
+    private val viewModel by viewModel<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookMeetingRoomTheme {
-               AppScaffold(shouldShowOnboarding = false)
+                val shouldShowOnboarding by viewModel.showOnBoarding.collectAsStateWithLifecycle()
+                AppScaffold(
+                    shouldShowOnboarding = shouldShowOnboarding
+                )
             }
         }
     }
