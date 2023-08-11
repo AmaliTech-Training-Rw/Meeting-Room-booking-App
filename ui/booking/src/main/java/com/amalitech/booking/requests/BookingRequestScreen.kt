7b60b1edf19fbea1d20e9231a68ddbf5b058d7af
Cadet Kamdaou <nkamdaou@gmail.com>
2023-08-11 12:14:31 +0200

    Added padding on booking the request screen

--- ui/booking/src/main/java/com/amalitech/booking/requests/BookingRequestScreen.kt
+++ ui/booking/src/main/java/com/amalitech/booking/requests/BookingRequestScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -79,7 +80,11 @@ fun BookingRequestScreen(
         )
     }
 
-    Box(modifier.fillMaxSize()) {
+    Box(
+        modifier
+            .fillMaxSize()
+            .padding(spacing.spaceMedium)
+    ) {
         if (uiState.isLoading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
