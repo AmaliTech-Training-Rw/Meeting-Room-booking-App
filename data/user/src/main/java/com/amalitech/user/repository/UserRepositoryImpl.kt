f1cca7f8aa9245b5af8f7581799817bda22bb58b
ma-za-kpe <makpalyy@gmail.com>
2023-08-23 20:59:14 +0300

    add more images

--- data/user/src/main/java/com/amalitech/user/repository/UserRepositoryImpl.kt
+++ data/user/src/main/java/com/amalitech/user/repository/UserRepositoryImpl.kt
@@ -57,6 +57,41 @@ class UserRepositoryImpl(
             "Katogo Mere",
             "katogo@gmail.com",
             false
+        ),
+        User(
+            "6",
+            "https://fastly.picsum.photos/id/433/200/200.jpg?hmac=dBn6DDBngOA94Grm3jfIJNDtv08GorUvB0zMeAw0Jfs",
+            "Kato Danny",
+            "kato@gmail.com",
+            true
+        ),
+        User(
+            "7",
+            "https://fastly.picsum.photos/id/98/200/200.jpg?hmac=QiHfqn1VEz1-KB7Wpb5iUbtrmTV8s2e3DJFb4HEp-y0",
+            "Sonia John",
+            "sonia@gmail.com",
+            false
+        ),
+        User(
+            "8",
+            "https://fastly.picsum.photos/id/1076/200/200.jpg?hmac=KTOq4o7b6rXzwd8kYN0nWrPIeKI97mzxBdWhnn-o-Nc",
+            "Elon Msk",
+            "elon@gmail.com",
+            true
+        ),
+        User(
+            "9",
+            "https://fastly.picsum.photos/id/961/200/200.jpg?hmac=gHwrXvhjUL97oGKmAYQn508wdQ_V5sE9P64erzR-Ork",
+            "Hesus Kovid",
+            "kovid@gmail.com",
+            true
+        ),
+        User(
+            "10",
+            "https://fastly.picsum.photos/id/866/200/200.jpg?hmac=i0ngmQOk9dRZEzhEosP31m_vQnKBQ9C19TBP1CGoIUA",
+            "Some Mane",
+            "mane@gmail.com",
+            false
         )
     )
 
