55af506a03a9e7820eb6ef0858ddfc44af860f31
ma-za-kpe <makpalyy@gmail.com>
2023-09-01 13:57:27 +0300

    clean up routes file

--- ui/user/src/main/java/com/amalitech/user/di/userPresentationModule.kt
+++ ui/user/src/main/java/com/amalitech/user/di/userPresentationModule.kt
@@ -3,6 +3,7 @@ package com.amalitech.user.di
 import com.amalitech.user.UserViewModel
 import com.amalitech.user.adduser.AddUserViewModel
 import com.amalitech.user.usecases.AddUserUseCase
+import com.amalitech.user.usecases.FetchRemoteUsersCase
 import com.amalitech.user.usecases.GetUserCase
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -18,6 +19,10 @@ val userPresentationModule = module {
 
     single {
         GetUserCase(get())
+    }
+
+    single {
+        FetchRemoteUsersCase(get())
     }
 
     single {
