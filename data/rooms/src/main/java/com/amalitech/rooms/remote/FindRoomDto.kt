569f4b5ba9a48e93489c22421d56a8a7252e0dcf
Kamdaou <nkamdaou@gmail.com>
2023-09-13 16:01:06 +0200

    Integrated update room

--- data/rooms/src/main/java/com/amalitech/rooms/remote/FindRoomDto.kt
+++
@@ -1,7 +0,0 @@
-package com.amalitech.rooms.remote
-
-import com.amalitech.core.data.data_source.remote.dto.RoomsData
-
-data class FindRoomDto(
-    val data: RoomsData
-)
--- data/rooms/src/main/java/com/amalitech/rooms/remote/RoomsApiService.kt
+++ data/rooms/src/main/java/com/amalitech/rooms/remote/RoomsApiService.kt
@@ -1,6 +1,7 @@
 package com.amalitech.rooms.remote
 
 import com.amalitech.core.data.data_source.remote.dto.ApiSuccessResponseDto
+import com.amalitech.core.data.data_source.remote.dto.FindRoomDto
 import com.amalitech.core.data.data_source.remote.dto.RoomsDto
 import okhttp3.MultipartBody
 import retrofit2.Response
