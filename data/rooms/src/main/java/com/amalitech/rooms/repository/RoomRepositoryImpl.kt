ab3426f99298c5568911dd7f0be0ade81099889f
Kamdaou <nkamdaou@gmail.com>
2023-09-15 13:39:01 +0200

    Removed code added while rebasing

--- data/rooms/src/main/java/com/amalitech/rooms/repository/RoomRepositoryImpl.kt
+++ data/rooms/src/main/java/com/amalitech/rooms/repository/RoomRepositoryImpl.kt
@@ -15,11 +15,6 @@ import com.amalitech.rooms.remote.RoomsApiService
 import com.amalitech.rooms.remote.dto.IntervalHour
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -215,85 +210,5 @@ class RoomRepositoryImpl(
         } catch (e: Exception) {
             ApiResult(error = e.extractError())
         }
-    }
-
-    private fun saveBitmapToFile(file: File): File {
-        return try {
-            // BitmapFactory options to downsize the image
-            val o = BitmapFactory.Options()
-            o.inJustDecodeBounds = true
-            o.inSampleSize = 6
-            // factor of downsizing the image
-            var inputStream = FileInputStream(file)
-            //Bitmap selectedBitmap = null;
-            BitmapFactory.decodeStream(inputStream, null, o)
-            inputStream.close()
-
-            // The new size we want to scale to
-            val REQUIRED_SIZE = 75
-
-            // Find the correct scale value. It should be the power of 2.
-            var scale = 1
-            while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
-                o.outHeight / scale / 2 >= REQUIRED_SIZE
-            ) {
-                scale *= 2
-            }
-            val o2 = BitmapFactory.Options()
-            o2.inSampleSize = scale
-            inputStream = FileInputStream(file)
-            val selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2)
-            inputStream.close()
-
-            // here i override the original image file
-            file.createNewFile()
-            val outputStream = FileOutputStream(file)
-            selectedBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            file
-        } catch (e: java.lang.Exception) {
-            file
-        }
-    }
-
-    @Throws(IOException::class)
-    fun getFile(context: Context, uri: Uri): File {
-        val destinationFilename =
-            File(context.filesDir.path + File.separatorChar + queryName(context, uri))
-        try {
-            context.contentResolver.openInputStream(uri).use { ins ->
-                createFileFromStream(
-                    ins!!,
-                    destinationFilename
-                )
-            }
-        } catch (ex: java.lang.Exception) {
-            ex.printStackTrace()
-        }
-        return destinationFilename
-    }
-
-    private fun createFileFromStream(ins: InputStream, destination: File?) {
-        try {
-            FileOutputStream(destination).use { os ->
-                val buffer = ByteArray(4096)
-                var length: Int
-                while (ins.read(buffer).also { length = it } > 0) {
-                    os.write(buffer, 0, length)
-                }
-                os.flush()
-            }
-        } catch (ex: java.lang.Exception) {
-            Log.e("Save File", ex.message!!)
-            ex.printStackTrace()
-        }
-    }
-
-    private fun queryName(context: Context, uri: Uri): String {
-        val returnCursor = context.contentResolver.query(uri, null, null, null, null)!!
-        val nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-        returnCursor.moveToFirst()
-        val name = returnCursor.getString(nameIndex)
-        returnCursor.close()
-        return name
     }
 }
